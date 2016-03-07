28ceaa16aeaa571fb154d1c093e4cc862234eea3
leaf <leftcold@gmail.com>
2016-03-07 23:48:32 +0800

    修改部分模块为typescript实现

--- _source/lib/command.ts
+++ _source/lib/command.ts
@@ -3,6 +3,11 @@ let program:commander.IExportedCommand = require('commander');
 let pkg:any = require('../package.json');
 let watchAction:any = require('./watch.js');
 
-module.exports = function (args:string):void {
-    program.version()
+module.exports = function (args:string[]):void {
+    program.version(pkg.version);
+    
+    program.command('watch', '', {isDefault: true})
+    .action(watchAction.run);
+    
+    program.parse(args);
 }
