6cd867564c2650e29a63d57f529044ccad92cc2f
leaf <leftcold@gmail.com>
2016-03-17 23:53:15 +0800

    根据输入计算配置文件相对路径

--- _source/lib/watch.ts
+++ _source/lib/watch.ts
@@ -1,6 +1,7 @@
 /// <reference path="../../typings/main.d.ts"/>
 /// <reference path="../declare/main.d.ts"/>
-let configUtil:any = require('./configUtil.js');
+let configUtil: any = require('./configUtil.js');
+let fileManage: any = require('./fileManage.js');
 import path = require('path');
 import child_process = require('child_process');
 import bs = require('browser-sync');
@@ -9,11 +10,14 @@ let exec = child_process.exec;
 
 class Watch {
     config: configStructure;
-    run(dir: string = '.', options: any): void {
+    run(dir: string = './', options: any): void {
+      let cliPath: string = process.cwd();
       let configFile = '_config';
       if (options.config && options.config != true) {
         configFile = options.config;
       }
+      configFile = path.normalize(cliPath + '/' + dir + configFile);
+      console.log(configFile);
       if (!this.config) {
         this.config = configUtil.getConfig(configFile);
       }
@@ -46,47 +50,16 @@ class Watch {
 
       return allow;
     }
-    getCompileCmdAndFileName(file: string, ext: string): Array<string> {
-      let config = this.config;
-      let result: Array<any> = [];
-      let fileName: string = path.basename(file, ext);
-      let filePath: string = path.dirname(file);
-      let relativePath: string = path.resolve(config.baseDir, filePath);
-
-      result.push(fileName);
-
-      if (path.sep != '/') {
-        relativePath = relativePath.replace('\\', '/');
-        file = file.replace('\\', '/');
-      }
-      let cmdDefine: any = this.config.define;
-      let pathNode: any = false;
-      let cmdArray: Array<string> = [];
-
-      if (cmdDefine[relativePath]) {
-        pathNode = cmdDefine[relativePath];
-      }
-
-      let cmdNode: any = pathNode["."+ext];
-      if (cmdNode) {
-        cmdArray = [].concat(cmdNode.command);
-        result.push(cmdArray);
-      }
-      else {
-        result = [];
-      }
-      return result;
-    }
-    compileTask(file: string, ext: string, reload: any): void {
-      let cmdAndFileName: Array<string> = this.getCompileCmdAndFileName(file, ext)
+    compileTask(file: string, reload: any): void {
+      let fileObject = fileManage.getFile(file);
+      let command: Array<string> = fileObject.command;
+      console.log(command);
     }
     compileCallback(file: string): void {
-      let ext: string = path.extname(file);
-
       if (this.checkIgnore(file) != true) {
         return;
       }
-      this.compileTask(file, ext, bs.reload);
+      this.compileTask(file, bs.reload);
     }
 }
 
