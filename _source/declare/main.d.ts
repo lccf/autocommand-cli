ff9e86975eab87814b89a10582c23da086a7e2d9
leaf <leftcold@gmail.com>
2016-03-24 22:09:43 +0800

    更新提示引用

--- _source/declare/main.d.ts
+++ _source/declare/main.d.ts
@@ -1,1 +1,5 @@
 /// <reference path="./config.d.ts"/>
+/// <reference path="../lib/command.ts"/>
+/// <reference path="../lib/configUtil.ts"/>
+/// <reference path="../lib/fileManage.ts"/>
+/// <reference path="../lib/watch.ts"/>
