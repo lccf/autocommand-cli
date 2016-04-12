d32aa9efb3261bf0887093c7815e717e2ab5778e
leaf <leftcold@gmail.com>
2016-04-12 23:34:19 +0800

    完善相对路径的匹配，修改变量的匹配规则

--- _source/lib/fileManage.ts
+++ _source/lib/fileManage.ts
@@ -76,13 +76,19 @@ class fileManage {
     if (path.sep != '/') {
       relativePath = relativePath.replace('\\', '/');
       file = file.replace('\\', '/');
+
     }
+
     let cmdDefine: any = config.define;
     let pathNode: any = false;
     let cmdArray: Array<string> = [];
     let variable: any = config.variable;
     if (cmdDefine[relativePath]) {
       pathNode = cmdDefine[relativePath];
+    }
+    else if (cmdDefine[relativePath+'/']) {
+      pathNode = cmdDefine[relativePath+'/'];
+
     }
     else {
       pathNode = cmdDefine;
@@ -114,14 +120,14 @@ class fileManage {
       cmdArray = [].concat(cmdNode.command);
       for(var i=0, j = cmdArray.length; i<j; i++) {
         let item: string = cmdArray[i];
-        item = item.replace(/\#\{\$([^}]+)\}/g, varReplace);
+        item = item.replace(/\#\{([^}]+)\}/g, varReplace);
         cmdArray[i] = item;
       }
       result = result.concat(cmdArray);
     }
     this._command = result;
     if (cmdNode.file) {
-      this._file = cmdNode.file.replace(/\#\{\$([^}]+)\}/g, varReplace);
+      this._file = cmdNode.file.replace(/\#\{([^}]+)\}/g, varReplace);
     }
   }
   /* 构造函数 */
